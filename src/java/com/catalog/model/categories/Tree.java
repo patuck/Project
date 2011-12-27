fcc5ab3507a7b2509f5e480ae6eba90492733f95
Reshad Patuck <reshadpatuck1@gmail.com>
2011-12-27 19:33:31 +0530

    Removed unused code from tree class

--- src/java/com/catalog/model/categories/Tree.java
+++ src/java/com/catalog/model/categories/Tree.java
@@ -77,59 +77,6 @@ public class Tree
         }
         return new ArrayList<Node>();
     }
-            
-    
-
-    
-    
-    public static void main (String [] args)
-    {
-        Tree t= new Tree("out.html");
-        t.addNode(new Node("1", "0", "Phones"));
-        t.addNode(new Node("2", "0", "Computers"));
-        t.addNode(new Node("3", "0", "Cameras"));
-        t.addNode(new Node("4", "1", "Smartphones"));
-        t.addNode(new Node("5", "1", "Feature phones"));
-        t.addNode(new Node("6", "2", "Laptops"));
-        t.addNode(new Node("7", "2", "Desktops"));
-        t.addNode(new Node("8", "4", "Nokia"));
-        t.addNode(new Node("9", "4", "Samsung"));
-        t.addNode(new Node("10", "4", "Apple"));
-        t.addNode(new Node("11", "4", "LG"));
-        t.addNode(new Node("12", "5", "Nokia"));
-        t.addNode(new Node("13", "5", "Samsung"));
-        t.addNode(new Node("14", "6", "Sony"));
-        t.addNode(new Node("15", "6", "Dell"));
-        t.addNode(new Node("16", "6", "Lenovo"));
-        t.addNode(new Node("17", "7", "Dell"));
-        t.addNode(new Node("18", "7", "Lenovo"));
-        t.addNode(new Node("19", "7", "Acer"));
-        t.addNode(new Node("20", "3", "DSLR"));
-        t.addNode(new Node("21", "3", "Pont and shot"));
-        t.addNode(new Node("22", "21", "Kodak"));
-        t.addNode(new Node("23", "20", "Nikon"));
-        t.addNode(new Node("24", "21", "Samsung"));
-        
-       /* ArrayList<Node> a=t.getList();
-        for(int i=0;i<a.size();i++)
-        {
-            System.out.println(a.get(i).getParentID()+"--"+a.get(i).getNodeID()+"--"+a.get(i).getData());
-        }
-        
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        */
-        t.printChildNodes("0");
-        
-        /*ArrayList<Node> l= t.getParentNodes(new Node("9", "4", "Samsung"));
-        for(int i=0;i<l.size();i++)
-        {
-            System.out.println(l.get(i).getData());
-        }
-        */
-    }
-    
     public void printChildNodes(String nodeID)
     {
         try 
